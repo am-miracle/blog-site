2e8859138f2cfa798a4561ffc9b53b0825b170a7
am-miracle <judemiraco0@gmail.com>
2022-02-05 23:43:01 +0100

    update header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,24 +1,27 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getCategories } from '../services';
 
-
-const Categories = [
-    {name: 'React', slug: 'react'},
-    {name: 'Web Development', slug: 'web-dev'},
-]
 
 const Header = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+       getCategories()
+        .then((newCategories)=> setCategories(newCategories))
+    },[]);
+
   return (
       <div className='container mx-auto px-10 mb-8'>
           <div className='border-b w-full inline-block border-blue-400 py-8'>
               <div className='md:float-left block'>
-                  <Link href='/'>
+                  <Link href='/' passHref>
                       <span className='cursor-pointer font-bold text-4xl text-white'>CMSBlog</span>
                   </Link>
               </div>
               <div className='hidden md:float-left md:contents'>
-                {Categories.map((category)=>(
-                    <Link key={category.slug} href={`/category/${category.slug}`}>
+                {categories.map((category)=>(
+                    <Link key={category.slug} href={`/category/${category.slug}`} passHref>
                         <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
                             {category.name}
                         </span>
